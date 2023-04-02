5372c40758231465b126d52257df3c1bdd85387e
61130061 <thezeroyou@gmail.com>
2023-04-02 18:59:26 +0700

    ➕ add our story page with active navbar color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,14 +1,23 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
 export default function Navbar () {
+  const router = useRouter();
+
   return (
-    <nav>
+    <nav className="border-b border-gray-200">
       <h1 className="text-center font-[200] tracking-[0.2em] my-10">Logo</h1>
 
-      <div className="max-w-4xl mx-auto flex items-center justify-between mb-5">
+      <div className="max-w-4xl mx-auto flex items-center justify-between mb-4">
         {/* Left menu */}
         <div className="flex gap-2">
-          <button className="px-8 py-3">Home</button>
+          <Link href="/">
+            <button className={`hover:text-primary-3 px-8 py-3${router.pathname === "/" ? " text-primary-3":""}`}>Home</button>
+          </Link>
           <button className="px-8 py-3">Shop Collection</button>
-          <button className="px-8 py-3">Our Story</button>
+          <Link href="/our-story">
+            <button className={`hover:text-primary-3 px-8 py-3${router.pathname === "/our-story" ? " text-red-500":""}`}>Our Story</button>
+          </Link>
           <button className="px-8 py-3">Contact</button>
         </div>
 
