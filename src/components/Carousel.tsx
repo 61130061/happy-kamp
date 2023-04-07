143f3988fc88c06ecccfd1ab3a4234a894c67295
61130061 <thezeroyou@gmail.com>
2023-04-07 23:35:45 +0700

    ♻️ update Carousel image priority loading & aria-label to increase lighthouse test score

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -42,13 +42,14 @@ export default function Carousel({ images }: PropsType) {
             className={`carousel-item absolute w-full h-full top-0 left-0 opacity-0 transition-opacity duration-500 ease-in-out ${currentIndex === index ? 'opacity-100' : ''
               }`}
           >
-            <Image width={640} height={640} src={image} alt={`Image ${index}`} className="object-cover w-full h-full" />
+            <Image priority width={640} height={640} src={image} alt={`Image ${index}`} className="object-cover w-full h-full" />
           </div>
         ))}
       </div>
       <div className="carousel-dots absolute bottom-7 left-0 right-0 flex justify-center mt-4">
         {images.map((_, index) => (
           <button
+            aria-label={"show-slide-"+index}
             key={index}
             className={`carousel-dot w-3 h-3 rounded-full mx-2 focus:outline-none ${currentIndex === index ? 'bg-gray-700' : 'bg-gray-300'
               }`}
@@ -56,12 +57,12 @@ export default function Carousel({ images }: PropsType) {
           ></button>
         ))}
       </div>
-      <button className="carousel-arrow-left absolute top-1/2 left-4 md:left-10 transform -translate-y-1/2 focus:outline-none" onClick={goToPrevious}>
+      <button aria-label="previous-slide" className="carousel-arrow-left absolute top-1/2 left-4 md:left-10 transform -translate-y-1/2 focus:outline-none" onClick={goToPrevious}>
         <svg viewBox="0 0 24 24" className="w-10 h-10 fill-current text-gray-700">
           <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L9.83 12l5.58-5.59z"></path>
         </svg>
       </button>
-      <button className="carousel-arrow-right absolute top-1/2 right-4 md:right-10 transform -translate-y-1/2 focus:outline-none" onClick={goToNext}>
+      <button aria-label="next-slide" className="carousel-arrow-right absolute top-1/2 right-4 md:right-10 transform -translate-y-1/2 focus:outline-none" onClick={goToNext}>
         <svg viewBox="0 0 24 24" className="w-10 h-10 fill-current text-gray-700">
           <path d="M8.59 16.59L10 18l6-6-6-6-1.41 1.41L14.17 12l-5.58 5.59z"></path>
         </svg>
